6e58eadfb46fd61a64bc7728a0f0f6e05e64c5d9
David Francis <dfrancis@mac-mini.local>
2015-08-17 22:28:18 -0700

    Update to use preconditionFailure() instead of assertionFailure() for abstract methods which must be overridden.

--- XMLPerformance/iTunesRSSParser.swift
+++ XMLPerformance/iTunesRSSParser.swift
@@ -43,15 +43,24 @@ class iTunesRSSParser: NSObject {
     var downloadDuration: NSTimeInterval = 0
     var totalDuration: NSTimeInterval = 0
     
+    // Subclasses must implement this method and return the appropriate name for their XMLParserType.
     class func parserName() -> String {
-        assertionFailure("Class method parserName not valid for abstract base class iTunesRSSParser")
-        return "Base Class"
+        preconditionFailure("Class method parserName not valid for abstract base class iTunesRSSParser")
     }
     
+    // Subclasses must implement this method and return the appropriate XMLParserType
     class var parserType: XMLParserType {
-        assertionFailure("Class method parserType not valid for abstract base class iTunesRSSParser")
-        return .Abstract
+        preconditionFailure("Class method parserType not valid for abstract base class iTunesRSSParser")
     }
+    
+    // Subclasses must implement this method. It will be invoked on a secondary thread to keep the application responsive.
+    // Although NSURLConnection is inherently asynchronous, the parsing can be quite CPU intensive on the device, so
+    // the user interface can be kept responsive by moving that work off the main thread. This does create additional
+    // complexity, as any code which interacts with the UI must then do so in a thread-safe manner.
+    func downloadAndParse(url: NSURL) {
+        preconditionFailure("Object is of abstract base class iTunesRSSParser")
+    }
+
     
     func start() {
         startTimeReference = NSDate.timeIntervalSinceReferenceDate()
@@ -61,15 +70,7 @@ class iTunesRSSParser: NSObject {
         NSThread.detachNewThreadSelector("downloadAndParse:", toTarget: self, withObject: url)
     }
     
-    // Subclasses must implement this method. It will be invoked on a secondary thread to keep the application responsive.
-    // Although NSURLConnection is inherently asynchronous, the parsing can be quite CPU intensive on the device, so
-    // the user interface can be kept responsive by moving that work off the main thread. This does create additional
-    // complexity, as any code which interacts with the UI must then do so in a thread-safe manner.
-    func downloadAndParse(url: NSURL) {
-        assertionFailure("Object is of abstract base class iTunesRSSParser")
-    }
 
-    
     // Subclasses should invoke these methods and let the superclass manage communication with the delegate.
     // Each of these methods must be invoked on the main thread.
     func downloadStarted() {
